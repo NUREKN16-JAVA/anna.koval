173c176dc602dd3115f266ae6569cc789842b62a
Koval Anna <anna.koval@nure.ua>
2018-11-22 15:42:03 +0200

    comment1

---
+++ src/main/java/ua/nure/kn/koval/usermanagement/User.java
@@ -0,0 +1,98 @@
+package ua.nure.kn.koval.usermanagement;
+
+import java.io.Serializable;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import  java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class User implements Serializable{
+    private static final long serialVersionUID = 4188062179191390064L;
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(dateOfBirth, user.dateOfBirth);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, firstName, lastName, dateOfBirth);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+
+    public String getFullName () {
+        return new StringBuilder(getLastName())
+                .append(", ")
+                .append(getFirstName())
+                .toString();
+    }
+
+    public long getAge(){
+        SimpleDateFormat simdatf=new SimpleDateFormat("dd-MM-yyyy");
+        DateTimeFormatter dattime = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate nowdat = LocalDate.now();
+        LocalDate birth=LocalDate.parse(simdatf.format(dateOfBirth),dattime);
+        Period between = Period.between(birth, nowdat);
+        return between.getYears();
+    }
+}
+
---
+++ src/test/java/ua/nure/kn/koval/usermanagement/UserTest.java
@@ -0,0 +1,71 @@
+package ua.nure.kn.koval.usermanagement;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    User user;
+    private static final String FIRST_NAME_ETALONE="Anna";
+    private static final String LAST_NAME_ETALONE="Koval";
+    private static final String FULL_NAME_ETALONE="Koval, Anna";
+
+   // private static final int CURRENT_YEAR=2018;
+    private static final int YEAR_OF_BIRTH=1999;
+    private static final int ETALONE_AGE=19;
+    private static final int DAY_OF_BIRTH=24;
+    private static final int MONTH_OF_BIRTH=Calendar.AUGUST;
+
+    @Before
+    public void setUp() throws Exception {
+        user=new User();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    //test getFullName
+    @Test
+    public void getFullName() {
+        user.setFirstName(FIRST_NAME_ETALONE);
+        user.setLastName(LAST_NAME_ETALONE);
+        assertEquals(FULL_NAME_ETALONE, user.getFullName());
+    }
+    //test getAge
+    @Test
+    public void testGetAge(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE, user.getAge());
+    }
+
+    // when birthday was one year ago
+    @Test
+    public void testYearAgo(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH-1, MONTH_OF_BIRTH, DAY_OF_BIRTH);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE+1, user.getAge());
+    }
+    @Test
+    public void testDayBeforeBirth()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, Calendar.getInstance().get(Calendar.MONTH) , DAY_OF_BIRTH-1);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE-1, user.getAge());
+    }
+    @Test
+    public void testTwoDayBeforeBirth()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH , DAY_OF_BIRTH+2);
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE, user.getAge());
+    }
+}
