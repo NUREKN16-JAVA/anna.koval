9c03c0124c2e1d21d924d3b5b58c4fc86783e151
Koval Anna <anna.koval@nure.ua>
2018-12-27 16:39:29 +0200

    Lab5
    comment5

---
+++ src/main/java/ua/nure/kn/koval/usermanagement/agent/SearchException.java
@@ -0,0 +1,24 @@
+package ua.nure.kn.koval.usermanagement.agent;
+
+public class SearchException extends Exception {
+
+	public SearchException() {
+		super();
+	}
+
+	public SearchException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public SearchException(String message) {
+		super(message);
+
+	}
+
+	public SearchException(Throwable cause) {
+		super(cause);
+
+	}
+
+}
